95b9ae3dbf7bd4dc2583d415ae2500593d914bea
ervitis <vitomarti@gmail.com>
2016-07-10 19:26:16 +0200

    fix: typo in parameter

--- bds/argparser.py
+++ bds/argparser.py
@@ -8,8 +8,8 @@ class DataServiceArgumentParser(object):
     def __init__(self):
         self._parser = ArgumentParser()
 
-    def parse_build(self, short, long, **kwargs):
-        self._parser.add_argument(short, long, **kwargs)
+    def parse_build(self, short_description, long_description, **kwargs):
+        self._parser.add_argument(short_description, long_description, **kwargs)
 
         return self
 
